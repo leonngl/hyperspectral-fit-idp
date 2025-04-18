b6583fa4570c022647d9b1dce45072332ef7ae63
Leon Nagel <leon.nagel@tum.de>
2025-04-18 21:41:04 +0200

    updated noteboot for simulation data

--- src/.ipynb_checkpoints/data_loader-checkpoint.py
+++ src/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -6,8 +6,53 @@ import scipy
 from spectral import open_image
 import imageio
 import mbll_functions
+import config
 
 class DataLoader():
+
+    # reference values in [1/cm]
+    params_ref_gray_matter = np.array([0.0646, 0.0114, 0.0064, 0.0016, 0.73, 0.1, 40.8, 3.089])
+    params_ref_gray_matter_fraction = mbll_functions.concentrations_to_blood_fraction(params_ref_gray_matter)
+    params_ref_blood_vessel = np.array([1.836, 0.488, 0, 0, 0.55, 0.01, 22.0, 0.660])
+    params_ref_blood_vessel_fraction = mbll_functions.concentrations_to_blood_fraction(params_ref_blood_vessel)
+
+    gray_matter_parameters = np.array([0.0646, 0.0114, 0.0064, 0.0016, 0.73, 0.1, 40.8, 3.089])
+
+    tissue_parameters = {
+        "gray matter": [ # Digital instrument paper
+            np.array([0.0646, 0.0114, 0.0064, 0.0016, 0.73, 0.1]),
+            np.array([40.8, 3.089]),
+            0.85,
+            1.36
+        ],
+        "artery": [ # Digital instrument paper
+            np.array([2.27752, 0.4648, 0, 0, 0.55, 0.01]),
+            np.array([22.0, 0.660]),
+            0.935,
+            1.4
+        ],
+        "vein": [ # Digital instrument paper
+            np.array([1.3944, 0.9296, 0, 0, 0.55, 0.01]),
+            np.array([22.0, 0.660]),
+            0.935,
+            1.4
+        ],
+        "blood vessel average": [ 
+            np.array([1.836, 0.488, 0, 0, 0.55, 0.01]),
+            np.array([22.0, 0.660]),
+            0.935,
+            1.4
+        ],
+        "tumor": [
+            # white matter should have approx. half of the absorption of gray matter, and white matter has very similar absorption to gray matter
+            np.array([0.0646, 0.0114, 0.0064, 0.0016, 0.73, 0.1]) / 2,
+            np.array([33.6, 1.712]), # Jacques Overview
+            0.96, # Madsen
+            1.4 # typical refractive index of brain tissue, also used by Poulon et. al
+        ]
+    }
+
+
     
     def __init__(self, path, wavelength_left_cut, wavelength_right_cut):
         assert(wavelength_left_cut < wavelength_right_cut)
@@ -20,15 +65,43 @@ class DataLoader():
         else:
             self.path = path
 
+        # instance method uses loader wavelengths
+        self.absorption_coefs = lambda use_diff_oxycco, use_water_and_fat: DataLoader.absorption_coefs(self.wavelengths, use_diff_oxycco, use_water_and_fat)
 
-        # reference values in [1/cm]
-        self.params_ref_gray_matter = np.array([0.0646, 0.0114, 0.0064, 0.0016, 0.73, 0.1, 40.8, 3.089])
-        self.params_ref_gray_matter_fraction = mbll_functions.concentrations_to_blood_fraction(self.params_ref_gray_matter)
-        self.params_ref_blood_vessel = np.array([1.836, 0.488, 0, 0, 0.55, 0.01, 22.0, 0.660])
-        self.params_ref_blood_vessel_fraction = mbll_functions.concentrations_to_blood_fraction(self.params_ref_blood_vessel)
 
-    # absorption coefficients in OD/(mM*cm)
-    def absorption_coefs(self, use_diff_oxycco=True, use_water_and_fat=False):
+    @staticmethod
+    def absorption_coefs(wavelengths, use_diff_oxycco=False, use_water_and_fat=True):
+        spectra_wavelengths = np.loadtxt(config.caredda_spectra / "lambda.txt")
+
+        if not np.all(np.diff(spectra_wavelengths) > 0):
+            raise ValueError("Error in spectra wavelenghts: Values not ascending.")
+    
+        if wavelengths[0] < spectra_wavelengths[0] or wavelengths[-1] > spectra_wavelengths[-1]:
+            raise ValueError("Requested wavelength outside of spectra wavelengths.")
+        
+        mu_a_matrix = np.empty((len(wavelengths), 4 + use_water_and_fat * 2))
+        
+        for i, mol in enumerate(["HbO2", "Hb", "oxCCO", "redCCO"]):
+            spectra_vals = np.loadtxt(config.caredda_spectra / f"eps_{mol}.txt") / 1e3 * np.log(10)
+            mu_a_matrix[:, i] = np.interp(wavelengths, spectra_wavelengths, spectra_vals)
+        
+        if use_diff_oxycco:
+            mu_a_matrix_diff = np.empty((len(wavelengths), mu_a_matrix.shape[1]-1))
+            mu_a_matrix_diff[:, :3] = mu_a_matrix[:, :3]
+            mu_a_matrix_diff[:, 2] -= mu_a_matrix[3]
+            mu_a_matrix = mu_a_matrix_diff
+        
+        if not use_water_and_fat:
+            return mu_a_matrix
+        
+        for i, mol in enumerate(["H2O", "Fat"]):
+            spectra_vals = np.loadtxt(config.caredda_spectra / f"mua_{mol}.txt")
+            mu_a_matrix[:, -2 + i] = np.interp(wavelengths, spectra_wavelengths, spectra_vals)
+        
+        return mu_a_matrix
+        
+    # absorption coefficients in 1/(mM*cm)
+    def absorption_coefs_old(self, use_diff_oxycco=True, use_water_and_fat=False):
         molecules, x = preprocessing.read_molecules(self.wavelength_left_cut, self.wavelength_right_cut, self.wavelengths)
         y_hbo2_f, y_hb_f, y_coxa, y_creda, y_water, y_fat = molecules
         if not (x == self.wavelengths).all():
@@ -54,13 +127,30 @@ class DataLoader():
                 np.asarray(y_water)[None, :].transpose(),
                 np.asarray(y_fat)[None, :].transpose(),
         ))
-
         
         return mu_a_matrix
 
     # wavelengths in nm, result in cm^-1
     def absorption_coef_background(self):
         return 7.84e8*self.wavelengths**(-3.255)
+
+    @staticmethod
+    def mu_a_func_gray_matter(wl):
+        mu_a_matrix = DataLoader.absorption_coefs(wl, use_diff_oxycco=False, use_water_and_fat=True)
+        return mu_a_matrix @ DataLoader.params_ref_gray_matter[:6]
+    
+    @staticmethod
+    def mu_a_func_blood_vessel(wl):
+        mu_a_matrix = DataLoader.absorption_coefs(wl, use_diff_oxycco=False, use_water_and_fat=True)
+        return mu_a_matrix @ DataLoader.params_ref_blood_vessel[:6]
+    
+    @staticmethod
+    def mu_s_red_func_gray_matter(wl):
+        return DataLoader.params_ref_gray_matter[-2] * np.power((wl/500), -DataLoader.params_ref_gray_matter[-1])
+    
+    @staticmethod
+    def mu_s_red_func_blood_vessel(wl):
+        return DataLoader.params_ref_blood_vessel[-2] * np.power((wl/500), -DataLoader.params_ref_blood_vessel[-1])
        
 
 class DataLoaderNIRS(DataLoader):
@@ -131,9 +221,7 @@ class DataLoaderHELICOID(DataLoader):
         super().__init__(path, wavelength_left_cut, wavelength_right_cut)
         self.patient_id = None
         self.num_wavelengths = num_wavelengths
-    
-    def load_data(self, patient_id):
-        
+
         self.wavelengths = np.linspace(400, 1000, 826)
         wavelength_idxs = (self.wavelengths >= self.wavelength_left_cut) & (self.wavelengths <= self.wavelength_right_cut)
         if self.num_wavelengths is not None:
@@ -142,8 +230,10 @@ class DataLoaderHELICOID(DataLoader):
             new_idxs = np.array([False] * len(wavelength_idxs))
             new_idxs[keep_idxs] = True
             wavelength_idxs &= new_idxs
+        self.wavelength_idxs = wavelength_idxs
         self.wavelengths = self.wavelengths[wavelength_idxs]
-
+    
+    def load_data(self, patient_id):
         p = self.path / patient_id
         raw_data = open_image(p / "raw.hdr").load()
         white_reference = open_image(p / "whiteReference.hdr").load()
@@ -151,7 +241,7 @@ class DataLoaderHELICOID(DataLoader):
         self.label_map = np.squeeze(open_image(p / "gtMap.hdr").load())
         self.reflectance = (raw_data - dark_reference) / (white_reference - dark_reference)
         self.reflectance = np.transpose(self.reflectance, (2, 0, 1)) # move wavelength axis to front
-        self.reflectance = self.reflectance[wavelength_idxs, ...] # choose desired wavelengths
+        self.reflectance = self.reflectance[self.wavelength_idxs, ...] # choose desired wavelengths
         self.reflectance[self.reflectance <= 0] = 1e-5 #prevent 0 value in log
 
         self.patient_id = patient_id
